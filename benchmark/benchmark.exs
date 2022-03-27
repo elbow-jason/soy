4f3c641d58c0b9b9fe211396a32e8b9dd04148a1
Jason Goldberger <jasongoldberger@gmail.com>
2022-03-26 23:06:06 -0700

    add benchee

--- benchmark/benchmark.exs
+++ benchmark/benchmark.exs
@@ -3,11 +3,15 @@ import Soy.TestHelpers
 nums = for i <- 1..1000 do
   to_string(i)
 end
-db = Soy.Native.open(tmp_dir(), %Soy.OpenOpts{})
-:ok = Soy.Native.put(db, "1", "1")
+db = Soy.Native.path_open_db(tmp_dir(), %Soy.OpenOpts{})
+:ok = Soy.Native.db_put(db, "1", "1")
 
 
 Benchee.run(%{
-  "put_10k"   => fn -> Enum.map(nums, fn k -> Soy.Native.put(db, k, k) end) end,
-  "fetch_10k" => fn -> Enum.map(nums, fn k -> Soy.Native.fetch(db, k) end) end
+  "put_10k"   => fn -> Enum.map(nums, fn k -> Soy.Native.db_put(db, k, k) end) end,
+  "fetch_10k" => fn -> Enum.map(nums, fn k -> Soy.Native.db_fetch(db, k) end) end,
+  "batch_10k" => fn ->
+    batch = Enum.map(nums, fn k -> {:put, k, k} end)
+    Soy.Native.db_batch(db, batch)
+  end,
 })
